4e507d22c73a1dc67907a2e2bad6c4a1c36ccf44
“Andrew-AM1” <andrewamoako1@gmail.com>
2023-02-21 13:46:05 -0800

    Adding 0-putvhar file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main- main function (prints putchar)
+ * main: main function
+ * description: prints putchar
+ * _putchar: print putchar
+ * return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
