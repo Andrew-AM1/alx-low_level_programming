f90744eb77b02525eaece4828c809c949c9b97c5
“Andrew-AM1” <andrewamoako1@gmail.com>
2023-03-06 07:50:07 -0800

    adding 0x07 files

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: initial segment.
+ * @accept: accepted bytes.
+ * Return: the number of accepted bytes.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int a;
+
+	while (*s)
+	{
+		for (a = 0; accept[a]; a++)
+		{
+			if (*s == accept[a])
+			{
+				n++;
+				break;
+			}
+			else if (accept[a + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
